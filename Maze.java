275b55f39d778c8886570cef34c99c3984530954
Eric Xu <ericxu343@gmail.com>
2020-05-06 22:22:26 -0400

    editing lab 7

--- Maze.java
+++ Maze.java
@@ -8,31 +8,15 @@ import java.util.Scanner;
 
 public class Maze {
 
-    // A maze is a rectangular array of cells. The reason we use arrays is that
-    // the maze has a fixed size, and arrays are the fastest when indexing by
-    // position, which is exactly what we do when we search a maze.
+
     private final Cell[][] cells;
 
     private Location initialRatLocation;
     private Location initialCheeseLocation;
     private boolean inputIsAllValid;
 
-    /**
-     * Builds and returns a new maze given a description in the form of an array
-     * of strings, one for each row of the maze, with each string containing o's
-     * and w's and r's and c's. o=Open space, w=Wall, r=Rat, c=Cheese.
-     *
-     * The maze must be rectangular and contain nothing but legal characters. There
-     * must be exactly one 'r' and exactly one 'c'.
-     *
-     * The constructor is private to force users to only construct mazes through one
-     * of the factory methods fromString, fromFile, or fromScanner.
-     */
+
     private Maze(String[] lines) {
-        // TODO: Fill this in. There is a lot to check for! The maze must be
-        // perfectly rectanglar, not contain any illegal characters, have exactly
-        // one rat (not less, not more), and have exactly one cheese (not less,
-        // not more).
 
         inputIsAllValid = true;
         int numOfUChars = 0;
@@ -114,10 +98,6 @@ public class Maze {
     }
 
     public static Maze fromScanner(final Scanner scanner) {
-        // TODO: Fill this in. You will want to read line-by-line from the scanner
-        // storing each line in an array of strings, then turn the list into
-        // an array and pass that to the Maze constructor. Return the newly
-        // constructed maze from this method.
 
         ArrayList<String> anArrayList = new ArrayList<String>();
         while (scanner.hasNextLine()){
@@ -132,54 +112,39 @@ public class Maze {
 
     }
 
-    /**
-     * A nice representation of a Location, so we don't have to litter our code
-     * with separate row and column variables! A location object bundles these
-     * two values together. It also includes a whole bunch of nice little methods
-     * so that our code reads nicely.
-     */
+
     public class Location {
         private final int row;
         private final int column;
 
         Location(final int row, final int column) {
-            // TODO: Fill this in, it's pretty easy.
             this.row = row;
             this.column = column;
         }
 
         boolean isInMaze() {
-            // TODO: Fill this in. Return whether the row and column is a legal
-            // position in this maze.
             return row >= 0 && row < getHeight() && column >= 0 && column < getWidth();
 
         }
 
         boolean canBeMovedTo() {
-            // TODO: Fill this in. You can move to a space only if it is inside the
-            // maze and the cell is open or contains the cheese.
             return isInMaze() && (contents() == Cell.OPEN || contents() == Cell.CHEESE);
-            // return true;
+
         }
-        // Do all the conditions here
+
         boolean hasCheese() {
-            // TODO: Fill this in. Returns whether the cell has the cheese. You can
-            // use the contents() method to help you here.
             return isInMaze() && contents() == Cell.CHEESE;
         }
-        // Reverse the position or index
+
         Location above() {
-            // TODO: Fill this in. It should return a new location whose coordinates
-            // are (1) the row above this location's row, and (2) the same column.
             int newRow = row - 1;
             int newCol = column;
             Location currentLoc = new Location(newRow, newCol);
             return currentLoc;
 
         }
-        // Reverse the position or index
+
         Location below() {
-            // TODO: Fill this in. Return the location directly below this one.
             int newRow = row + 1;
             int newCol = column;
             Location currentLoc = new Location(newRow, newCol);
@@ -187,7 +152,6 @@ public class Maze {
         }
 
         Location toTheLeft() {
-            // TODO: Fill this in. Return the location directly to the left of this one.
             int newRow = row;
             int newCol = column - 1;
             Location currentLoc = new Location(newRow, newCol);
@@ -195,7 +159,6 @@ public class Maze {
         }
 
         Location toTheRight() {
-            // TODO: Fill this in. Return the location directly to the right of this one.
             int newRow = row;
             int newCol = column + 1;
             Location currentLoc = new Location(newRow, newCol);
@@ -211,23 +174,15 @@ public class Maze {
         }
 
         boolean isAt(final Location other) {
-            // TODO: Fill this in. Returns whether this location and the other location have
-            // the same row and column values.
             return (other.row == row) && (other.column == column);
 
         }
     }
 
-    /**
-     * A simple cell value. A cell can be open (meaning a rat has never visited it),
-     * a wall, part of the rat's current path, or "tried" (meaning the rat found it
-     * to be part of a dead end.
-     */
+
     public static enum Cell {
         OPEN(' '), WALL('\u2588'), TRIED('x'), PATH('.'), RAT('r'), CHEESE('c');
 
-        // This needs a constructor and a toString method. You might need to do some
-        // research on Java enums.
         private char shortCode;
 
         Cell(char code) {
@@ -238,7 +193,6 @@ public class Maze {
           return Character.toString(shortCode);
         }
 
-        // convert character to String
 
     }
 
@@ -247,29 +201,23 @@ public class Maze {
     }
 
     public int getWidth() {
-        // TODO: Fill this in. The information comes from the cells array.
         return cells[0].length;
     }
 
     public int getHeight() {
-        // TODO: Fill this in
         return cells.length;
     }
 
     public Location getInitialRatPosition() {
-        // TODO: Fill this in. It is a typical getter, since you already have a field
-        // for the initial rat position.
+
         return initialRatLocation;
     }
 
     public Location getInitialCheesePosition() {
-        // TODO: Fill this in
         return initialCheeseLocation;
     }
 
-    /**
-     * Returns a textual description of the maze, separating each row with a newline.
-     */
+
     public String toString() {
         return Stream.of(cells)
             .map(row -> Stream.of(row).map(Cell::toString).collect(joining()))
--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This repository contains some unfinished Java classes for you to complete.
 Import this repository into a new repository of your own and write the code according to the lab [instructions](https://cs.lmu.edu/~ray/classes/plab/lab/7/). Use the provided test scripts as you complete the programs.
 
 In your new repository, don’t forget to completely edit this README file, as _your_ repository will not contain any “unfinished Java programs.” Consider adding a fun image, but not _too_ fun, as your GitHub repository will be checked by prospective employers.
+
+MY ACTIONS:
+
+I filled created a maze in the Maze Class where we set the parameters. With the cell blocks, we set them to enums. We made sure the number of rats and cheese could not be one and we denoted them with 'r' and 'c.' We also created walls and open spaces with 'w' and 'o.' We also scanned files into the scanner which could be used to test later. I also created a BacktrackingMazeSolver which uses the backtracking algorithm to help solve the maze problem. We also created a listener to let the programmer know of an event. If the rat already went on the path, we set it equal to tried. Once it reached the cheese, then it achieved its goal. 
